c242ef17cc6f205148ef3d8ea700c571ce3d8cb2
Karen Baghdasaryan <karenbaghdasaryan598@gmail.com>
2022-04-03 15:35:55 +0400

    Init Commit

--- app/Http/Controllers/v1/API/Bot/BotController.php
+++ app/Http/Controllers/v1/API/Bot/BotController.php
@@ -22,7 +22,7 @@ class BotController extends Controller
         $bot->handleGetUpdates();
         $bot->useGetUpdatesWithoutDatabase();
         Log::critical('Test Message');
-        Log::critical('Hook message', [321]);
+        Log::info('Hook message', [321]);
         $bot->setUpdateFilter(function (Update $update, Telegram $telegram, &$reason = 'Update denied by update_filter') {
             Log::critical('Hook message', [$update]);
         });
@@ -34,6 +34,7 @@ class BotController extends Controller
         $bot = new Telegram('5198795597:AAGmCvaioJOhg1PSezP9IOMGiYYMfv5QeQ8', 'testbotorconstructorbot');
         Storage::disk('local')->put('logs/file.txt', 'webhook arrived');
         \Longman\TelegramBot\Request::sendMessage(['text' => '321654']);
+        Log::info('Hook message', [321]);
 
         $bot->setUpdateFilter(function (Update $update, Telegram $telegram, &$reason = 'Update denied by update_filter') {
             Log::critical('Hook message', [$update]);
